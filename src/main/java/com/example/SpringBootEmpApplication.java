10b3f030fcbaead2aa7920e5b398566c0b8dc2a0
swethaathuluru <84894164+swethaathuluru@users.noreply.github.com>
2021-06-02 11:10:09 +0530

    Update SpringBootEmpApplication.java

--- src/main/java/com/example/SpringBootEmpApplication.java
+++ src/main/java/com/example/SpringBootEmpApplication.java
@@ -5,16 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import com.example.beans.Employee;
+
 
 @SpringBootApplication
 public class SpringBootEmpApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context=SpringApplication.run(SpringBootEmpApplication.class, args);
-//		Employee e=context.getBean(Employee.class);
-//		e.setName("abc");
-//		System.out.println(e);
+
 	}
 	@Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
